49319020850d444d5e11f8dba56b6844bd00e2bb
MykhailoZhurba <mykhailo.zhurba0@gmail.com>
2026-09-25 09:26:13 +0000

    Deploying to gh-pages from @ MykhailoZhurba/ruina.photos@341ba9c04ee2f71136bde4c9da27e26f0354e577 🚀

--- _worker.js/index.js
+++ _worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_S-qs8H_3.mjs';
-import { manifest } from './manifest_C6lLxpg_.mjs';
+import { manifest } from './manifest_CtIWJJES.mjs';
 
 const serverIslandMap = new Map();;
 
